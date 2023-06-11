b90d6f9022ef8936ca37a37acaa5aec4a18f418f
asdkeks <4svivan@gmail.com>
2023-06-11 18:31:03 +0300

    Add Point class

--- sandbox/src/main/java/ru/stqa/pfa/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pfa/sandbox/MyFirstProgram.java
@@ -2,8 +2,10 @@ package ru.stqa.pfa.sandbox;
 
 public class MyFirstProgram {
 
-  public static void main(String[] args) {
-    System.out.println("Hello, world");
-  }
+    public static void main(String[] args) {
+        Point point1 = new Point(4,5);
+        Point point2 = new Point(6, 1);
+        System.out.println(point2.getDistance(point1, point2));
+    }
 
 }
---
+++ sandbox/src/main/java/ru/stqa/pfa/sandbox/Point.java
@@ -0,0 +1,15 @@
+package ru.stqa.pfa.sandbox;
+
+public class Point {
+
+    double xCoordinate;
+    double yCoordinate;
+    public Point (double xCoordinate, double yCoordinate) {
+        this.xCoordinate = xCoordinate;
+        this.yCoordinate = yCoordinate;
+    }
+
+    public double getDistance(Point point1, Point point2){
+        return Math.sqrt(Math.pow((point1.xCoordinate - point2.xCoordinate), 2) + Math.pow((point1.yCoordinate - point2.yCoordinate),2));
+    }
+}
