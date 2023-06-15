c7af2489fef3748c28848e285a88bdf73dd8d366
asdkeks <4svivan@gmail.com>
2023-06-15 19:00:19 +0300

    Solution change

--- sandbox/src/main/java/ru/stqa/pfa/sandbox/Solution.java
+++ sandbox/src/main/java/ru/stqa/pfa/sandbox/Solution.java
@@ -1,23 +1,17 @@
 package ru.stqa.pfa.sandbox;
 
+import java.util.Arrays;
+
 public class Solution {
     public static int[] twoSum(int[] nums, int target) {
-        int[] answer = new int[2];
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 if (nums[i] + nums[j] == target) {
-                    answer[0] = i;
-                    answer[1] = j;
+                    int[] answer = {i, j};
                     return answer;
                 }
             }
-//            if ((nums[answer[0]] + nums[answer[1]] == target)&&(answer[0] != answer[1])) {
-//                break;
-//            }
-//            else {
-//                continue;
-//            }
         }
-        return answer;
+        return null;
     }
 }
