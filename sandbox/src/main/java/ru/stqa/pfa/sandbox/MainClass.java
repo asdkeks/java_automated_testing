7dffea8be68359809fc7f8e782b80523c02b9598
asdkeks <4svivan@gmail.com>
2023-06-18 17:54:21 +0300

    Some new test + solution for LeetCode problems

--- sandbox/src/main/java/ru/stqa/pfa/sandbox/MainClass.java
+++ sandbox/src/main/java/ru/stqa/pfa/sandbox/MainClass.java
@@ -1,18 +1,20 @@
 package ru.stqa.pfa.sandbox;
 
 
+import java.util.Arrays;
+
 public class MainClass {
 
     public static void main(String[] args) {
-//        Point point1 = new Point(0, 3);
-//        Point point2 = new Point(4, 0);
-//        System.out.println(Point.getDistance(point1, point2));
-//
-//        int[] nums = {3, 2, 4};
-//        int target = 6;
-//        System.out.println(Arrays.toString((TwoSumSolution.twoSum(nums, target))));
-//
-//        System.out.println((RomanToIntegerSolution.romanToInt("MCMXCIV")));
+        Point point1 = new Point(0, 3);
+        Point point2 = new Point(4, 0);
+        System.out.println(Point.getDistance(point1, point2));
+
+        int[] nums = {3, 2, 4};
+        int target = 6;
+        System.out.println(Arrays.toString((TwoSumSolution.twoSum(nums, target))));
+
+        System.out.println((RomanToIntegerSolution.romanToInt("MCMXCIV")));
 
         System.out.println(LongestCommonPrefixSolution.longestCommonPrefix(new String[]{"flower","flowe", "flowers"}));
     }
