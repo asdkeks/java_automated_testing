0db1a52f0a814b0db1aecc62d54697a9d8dc3879
asdkeks <4svivan@gmail.com>
2023-07-08 19:52:45 +0300

    Group tests refactor. add @BeforeMethod init and refator code for group modify

--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -1,19 +1,22 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.util.List;
 
 public class GroupDeletionTests extends TestBase {
-
-    @Test
-    public void testGroupDeletion(){
+    @BeforeMethod
+    public void ensurePreconditions() {
         app.getNavigationHelper().goToGroupPage();
         if (app.getGroupHelper().isThereNoGroup()) {
             app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
         }
+    }
+    @Test
+    public void testGroupDeletion(){
         List<GroupData> before = app.getGroupHelper().getGroupList();
         app.getGroupHelper().selectGroup(0);
         app.getGroupHelper().deleteSelectedGroups();
