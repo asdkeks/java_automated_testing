899992aa74cbfb98fc52667de1e561c3c924d125
asdkeks <4svivan@gmail.com>
2023-07-19 23:07:47 +0300

    Add logback for logging tests info

--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,12 +1,18 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.openqa.selenium.remote.Browser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 
 import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
 public class TestBase {
+    Logger logger = LoggerFactory.getLogger(GroupCreationTests.class);
 
     protected static final ApplicationManager app =
             new ApplicationManager(System.getProperty("browser", Browser.CHROME.browserName()));
@@ -21,4 +27,13 @@ public class TestBase {
         app.stop();
     }
 
+    @BeforeMethod
+    public void logStartTest (Method m, Object[] p) {
+        logger.info("Start test " + m.getName() + " with parameters " + Arrays.asList(p));
+    }
+
+    @AfterMethod
+    public void logFinishTest (Method m, Object[] p) {
+        logger.info("Finish test " + m.getName());
+    }
 }
