759127fa3430d9f4839e63ef1d7b1fd2efb514ad
asdkeks <4svivan@gmail.com>
2023-08-06 19:28:19 +0300

    fixed test

--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/ContactInfoPageTest.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/ContactInfoPageTest.java
@@ -33,7 +33,7 @@ public class ContactInfoPageTest extends TestBase {
         app.goTo().homePage();
         String contactDataFromInfoPage =  app.contact().getDataFromInfoPage(contact);
 
-        assertThat(contactDataFromInfoPage, equalTo(mergeContactDataToInfoPage(contactInfoFromEditPage)));
+        assertThat(mergeContactDataToInfoPage(contactInfoFromEditPage), equalTo(contactDataFromInfoPage));
     }
 
     private String mergeContactDataToInfoPage(ContactData contact) {
