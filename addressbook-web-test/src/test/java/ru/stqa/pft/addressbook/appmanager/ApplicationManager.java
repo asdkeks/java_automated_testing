01c77fd90e4639fb6ec7e782317f67d27a00e87d
asdkeks <4svivan@gmail.com>
2023-08-07 22:09:00 +0300

    add selenium remote server

--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,15 +1,19 @@
 package ru.stqa.pft.addressbook.appmanager;
 
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.Browser;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
@@ -37,13 +41,21 @@ public class ApplicationManager {
         properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
         //Browser browser = Browser.CHROME;
         dbHelper = new DBHelper();
-        if (browser.equals(Browser.CHROME.browserName())) {
-            driver = new ChromeDriver();
-        } else if (browser.equals(Browser.FIREFOX.browserName())) {
-            driver = new FirefoxDriver();
-        } else if (browser.equals(Browser.IE.browserName())){
-            driver = new InternetExplorerDriver();
+
+        if ("".equals(properties.getProperty("selenium.server"))) {
+            if (browser.equals(Browser.CHROME.browserName())) {
+                driver = new ChromeDriver();
+            } else if (browser.equals(Browser.FIREFOX.browserName())) {
+                driver = new FirefoxDriver();
+            } else if (browser.equals(Browser.IE.browserName())) {
+                driver = new InternetExplorerDriver();
+            }
+        } else {
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setBrowserName(browser);
+            driver = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
         }
+
         //driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
         driver.get(properties.getProperty("web.baseUrl"));
         js = (JavascriptExecutor) driver;
